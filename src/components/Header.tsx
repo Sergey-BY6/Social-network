3976f327373b11d1b7a28491714e4c423bbafcd6
Sergey Babich <sergei.babich2022@yandex.by>
2023-01-05 22:35:20 +0300

    Create Header, Navbar and Profile component.

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+            <header className="header">
+                <img
+                    src="https://avatars.mds.yandex.net/i?id=8e1656b53d712f3d1d39bc3ecb78c46e4d0c80fc-8196573-images-thumbs&n=13" alt="image"/>
+            </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+
+            <div>
+                <img src="https://photoclub.by/images/main37/378920_main.jpg" alt="image"/>
+            </div>
+
+            <div>
+                ava+description
+            </div>
+
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Profile;
