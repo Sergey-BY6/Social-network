107aa0ee2b75bfe386f87b443c9d1e4ebb838b2c
Sergey Babich <sergei.babich2022@yandex.by>
2023-03-04 19:07:36 +0300

    54 lessons. Axios in ComponentDidMount.

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import {mapStateToPropsType, UsersPropsType} from './UsersContainer';
-import s from "./Users.module.css"
+import s from './Users.module.css'
 import axios from 'axios';
-import userPhoto from "./../../assets/images/user.jpg"
+import userPhoto from './../../assets/images/user.jpg'
 
 
 class Users extends React.Component<UsersPropsType> {
 
 
-    constructor(props: UsersPropsType) {
-        super(props);
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                console.log(response)
-                this.props.setUsers(response.data.items)
-            })
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            console.log(response)
+            this.props.setUsers(response.data.items)
+        })
     }
+
 
     render() {
         return (
@@ -23,12 +23,13 @@ class Users extends React.Component<UsersPropsType> {
                     this.props.usersPage.users.map(el => <div key={el.id}>
                     <span>
                         <div>
-                            <img src={el.photos.small !== null ? el.photos.small : userPhoto} className={s.userPhoto} alt="paint"/>
+                            <img src={el.photos.small !== null ? el.photos.small : userPhoto} className={s.userPhoto}
+                                 alt="paint"/>
                         </div>
                         <div>
                                  {el.followed
-                                     ? <button onClick={()=> this.props.unfollow(el.id)}>UnFollow</button>
-                                     : <button onClick={()=> this.props.follow(el.id)}>Follow</button>}
+                                     ? <button onClick={() => this.props.unfollow(el.id)}>UnFollow</button>
+                                     : <button onClick={() => this.props.follow(el.id)}>Follow</button>}
                         </div>
                     </span>
                         <span>
@@ -37,8 +38,8 @@ class Users extends React.Component<UsersPropsType> {
                             <div>{el.status}</div>
                         </span>
                         <span>
-                            <div>{"el.location.country"}</div>
-                            <div>{"el.location.city"}</div>
+                            <div>{'el.location.country'}</div>
+                            <div>{'el.location.city'}</div>
                         </span>
                     </span>
                     </div>)
